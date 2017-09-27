54277c59177d9a601e47edfa015f528b2b130012
dalespiteri <dalespiteri@gmail.com>
2017-09-26 21:25:12 -0400

    js fix and class rename

--- script/coverLetter.js
+++ script/coverLetter.js
@@ -1,7 +1,9 @@
 function othername() {
-    var input = document.getElementById("userInput").value;
-    document.getElementById("your-name").innerHTML = input;
-    $(".name-input").fadeOut(200);
-    $('.avatar').addClass('slide');
-    $('.avatar img').addClass('roll');
+    
+        var input = document.getElementById("userInput").value;
+        document.getElementById("your-name").innerHTML = input;
+        $('.name-input').fadeOut(200);
+        $('.avatar').addClass('slide');
+        $('.avatar img').addClass('roll');
+
 }
