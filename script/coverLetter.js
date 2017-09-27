a2c4636a2e9f04097c2eaeb06bfd4f5246a177fd
dalespiteri <dalespiteri@gmail.com>
2017-09-26 21:12:07 -0400

    Added name form

--- script/coverLetter.js
+++ script/coverLetter.js
@@ -1,5 +1,7 @@
-document.getElementById('submit-button').click(function () {
-    var enteredName = document.getElementById('entered-name').value;
-    console.log(enteredName);
-})
-
+function othername() {
+    var input = document.getElementById("userInput").value;
+    document.getElementById("your-name").innerHTML = input;
+    $(".name-input").fadeOut(200);
+    $('.avatar').addClass('slide');
+    $('.avatar img').addClass('roll');
+}
