5138d6be0db227c8c8cca0d9b13efd5249ff65c8
dalespiteri <dalespiteri@gmail.com>
2017-10-06 02:12:14 -0400

    js improvements
    switch from innerhtml to text content
    
    add a fallback for an empty input field
    
    add the ability to submit with enter key

--- script/coverLetter.js
+++ script/coverLetter.js
@@ -4,8 +4,16 @@
 function othername() {
   "use strict";
   var input = document.getElementById("userInput").value;
-  document.getElementById("your-name").innerHTML = input;
+  if (input != '') {
+    document.getElementById("your-name").textContent = input;
+  }
   $('.name-input').fadeOut(200);
   $('.avatar').addClass('slide');
   $('.avatar img').addClass('roll');
 }
+
+document.getElementById("userInput").onkeypress = function(e) {
+  if (e.keyCode === 13) {
+    othername();
+  }
+};
