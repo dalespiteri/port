74dac2de5197b98100e14dc4e5a287aea7d2675f
dalespiteri <dalespiteri@gmail.com>
2018-04-04 00:42:01 -0400

    header mobile adjustments

--- uiblog/script/blogScript.js
+++ uiblog/script/blogScript.js
@@ -1,6 +1,6 @@
 window.onscroll = () => {
   const nav = document.querySelector('.header');
-  if(this.scrollY <= 125) nav.className = 'header'; else nav.className = 'header scrolledHeader';
+  if(this.scrollY <= 100) nav.className = 'header'; else nav.className = 'header scrolledHeader';
 };
 
 $('.thumb001').click(function(){
