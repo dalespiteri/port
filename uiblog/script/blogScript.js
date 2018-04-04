35a400aa408f4c3c06a0de2fe771051408173a8c
dalespiteri <dalespiteri@gmail.com>
2018-04-03 23:08:10 -0400

    mobile friendly blog design

--- uiblog/script/blogScript.js
+++ uiblog/script/blogScript.js
@@ -2,3 +2,8 @@ window.onscroll = () => {
   const nav = document.querySelector('.header');
   if(this.scrollY <= 125) nav.className = 'header'; else nav.className = 'header scrolledHeader';
 };
+
+$('.thumb001').click(function(){
+  $('.pictureDisplay').toggleClass('pictureDisplayToggle');
+  $('.pictureDisplayImage').attr('src', "./images/blogThumbs/UIDailey001.png");
+});
